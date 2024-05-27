bcfcc9715334b33528b96d7174d7357ea967d38c
hansikaweerasena <hansikam94@gmail.com>
2024-05-27 18:30:50 -0400

    update shell script file

--- examples/blinky/blinky.sh
+++ examples/blinky/blinky.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
-yosys blinky.ys
-../../../nextpnr-ice40 --hx8k --json blinky.json --pcf blinky.pcf --asc blinky.asc --package cb132
+yosys -p 'synth_ice40 -top blinky -json blinky.json' blinky.v
+nextpnr-ice40 --hx8k --json blinky.json --pcf blinky.pcf --asc blinky.asc --package cb132
 icepack blinky.asc blinky.bin
 icebox_vlog blinky.asc > blinky_chip.v
 iverilog -o blinky_tb blinky_chip.v blinky_tb.v
